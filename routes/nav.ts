8b3ff0930808d9998fb63a3f0437e6860d3268ff
qiuyanxi <qiuyanxi1992@gmail.com>
2022-06-08 23:45:36 +0800

    docs: add javascript,flutter docs and update navs

--- routes/nav.ts
+++ routes/nav.ts
@@ -1,8 +1,34 @@
 const nav = [
-  null,
   {
-    title: 'GitHub',
-    path: 'https://github.com/18888628835',
+    title: '核心',
+    children: [
+      { title: 'CSS', path: '/css' },
+      { title: 'JavaScript', path: '/javascript' },
+      { title: '浏览器', path: '/浏览器' },
+      { title: 'HTTP', path: '/http' },
+      { title: 'Node', path: '/node' },
+      { title: 'Flutter', path: '/flutter' },
+    ],
   },
+  {
+    title: '杂项',
+    path: '/其他',
+    children: [
+      { title: '前端可视化', path: '/其他/前端可视化' },
+      { title: '前端性能优化', path: '/其他/前端性能优化' },
+      { title: '前端工程化', path: '/其他/前端工程化' },
+      { title: 'Webpack', path: '/其他/webpack' },
+      { title: 'Babel', path: '/其他/babel' },
+      { title: 'Docker', path: '/其他/docker' },
+      { title: 'Git', path: '/其他/git' },
+      { title: 'GraphQL', path: '/其他/graphql' },
+    ],
+  },
+  { title: '组件', path: '/components' },
+  {
+    title: 'package',
+    path: 'https://18888628835.github.io/react-drag-resizable/',
+  },
+  { title: 'GitHub', path: 'https://github.com/18888628835' },
 ];
 export default nav;
